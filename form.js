b5cbcc2a02fc8a20fa163c46a23f4ebcb3a56d18
Anamika Pandey <83291071+anamika62@users.noreply.github.com>
2021-11-12 21:01:00 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,8 +1,40 @@
-console.log('button');
 let btn = document.getElementById('btn');
-let Name = document.getElementById('Name');
 btn.addEventListener('click',function(e){
-    //a =prompt('Wants to submit');
+  let Name1 =document.getElementById('Name1');
+  let Name2 =document.getElementById('Name2');
+  let Name3 =document.getElementById('Name3');
+  let gender =document.getElementById('gender');
+  let email =document.getElementById('email');
+  let pincode =document.getElementById('pincode');
+  let info = localStorage.getItem("info");
+
+  if(info == null)
+   {
+     infoObj =[];
+   }
+  else
+   {
+     infoObj =JSON.parse(info);
+   }
+
+  let myObj ={
+    firstName :Name1.value,
+    LastName :Name2.value,
+    Education:Name3.value,
+    gender: gender.value,
+    Email:email.value,
+    pincode: pincode.value
+  }
+  infoObj.push(myObj);
+  localStorage.setItem("info", JSON.stringify(infoObj));
+
+  Name1.value ="";
+  Name2.value ="";
+  Name3.value ="";
+  gender.value="";
+  email.value="";
+  pincode.value="";
+ //a =prompt('Wants to submit');
     if (confirm('Wants to Submit')) {
         // Save it!
         console.log('saved');
@@ -10,19 +42,7 @@ btn.addEventListener('click',function(e){
         // Do nothing!
         console.log('not saved');
       }
+
+     alert('Thank You For Your Response:-)') ;
       
     });  
-    
-    localStorage.setItem('Name',Name);
-     // function saving();
-
-// let Name = document.getElementById('Name');
-// window.localStorage.setItem('Name');
-// let Name = localStorage.getItem("notes");
-//   if (Name == null) {
-//     NameObj = [];
-//   } else {
-//     NameObj = JSON.parse(notes);
-//   }
-//   console.log(Name);
-
